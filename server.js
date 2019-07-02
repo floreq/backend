060ffd76bbaedbdd99555b5bc251e5269708f762
Piotr Florczak <piotrflorczakk@gmail.com>
2019-07-02 22:02:19 +0200

    Dodanie polaczenia z baza danych

--- server.js
+++ server.js
@@ -5,59 +5,108 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3001;
 
-const db = new sqlite3.Database("frontendDB.sqlite3");
-
+// "Rozpakowywanie" przychodzacych zapytan
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 // Whitelist adresow
 const corsOptions = {
   origin: "http://localhost:3000",
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
-
+// Ogolna konfiguracja app
 app.use(cors(corsOptions));
 app.use(bodyParser());
 
-app.get("/api/tasks", (req, res) => {
+// Podlaczenie bazy
+let db = new sqlite3.Database("frontendDB.sqlite3", err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Connected to the in-memory SQlite database.");
+});
+
+const sql = "SELECT * FROM tasks ORDER BY id DESC";
+const tasks = [];
+
+db.all(sql, [], (err, rows) => {
+  if (err) {
+    throw err;
+  }
+  rows.forEach(row => {
+    tasks.push({
+      id: row.id,
+      actionDate: row.action_date,
+      createdAt: row.created_at,
+      deletedAt: row.deleted_at,
+      task: row.task,
+      comment: row.comment,
+      expense: row.expense,
+      quantity: row.quantity,
+      metalType: row.metal_type
+    });
+  });
+});
+
+// close the database connection
+db.close(err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Close the database connection.");
+});
+
+const tasks1 = [
+  {
+    id: 1,
+    actionDate: "21.06.2019",
+    createdAt: "",
+    deletedAt: "",
+    task: "zakup",
+    comment: "",
+    expense: "2251",
+    quantity: "151",
+    metalType: "stalowy",
+    origin: "Sklep 1"
+  },
+  {
+    id: 2,
+    actionDate: "21.06.2019",
+    createdAt: "",
+    deletedAt: "",
+    task: "odbior",
+    comment: "",
+    expense: "1201",
+    quantity: "51",
+    metalType: "stalowy",
+    origin: "Sklep 1"
+  },
+  {
+    id: 3,
+    actionDate: "21.06.2019",
+    createdAt: "",
+    deletedAt: "",
+    task: "odbior",
+    comment: "",
+    expense: "3000",
+    quantity: "3000",
+    metalType: "stalowy",
+    origin: "Sklep 1"
+  }
+];
+
+// Zapytania
+app.get("/tasks", (req, res) => {
   res.writeHead(200, { "Content-Type": "application/json" });
-  const tasks = [
-    {
-      id: 1,
-      actionDate: "21.06.2019",
-      createdAt: "",
-      deletedAt: "",
-      task: "zakup",
-      comment: "",
-      expense: "2251",
-      quantity: "151",
-      metalType: "stalowy",
-      origin: "Sklep 1"
-    },
-    {
-      id: 2,
-      actionDate: "22.06.2019",
-      createdAt: "",
-      deletedAt: "",
-      task: "odbior",
-      comment: "",
-      expense: "1201",
-      quantity: "51",
-      metalType: "stalowy",
-      origin: "Sklep 1"
-    }
-  ];
   res.end(JSON.stringify(tasks));
 });
 
-// app.post("/api/tasks", (req, res) => {
-//   res.writeHead(200, { "Content-Type": "application/json" });
-//   // Walidacja potrzebna
-//   console.log(req.body);
+app.post("/tasks", urlencodedParser, (req, res) => {
+  // Dodac walidacje
+  // Dodac w obiekcie created_at
+  console.log(req.body);
 
-//   db.run(`INSERT INTO a (b) VALUES ("${req.body.hello}")`);
-
-//   const x = { id: 1, name: "abc" };
-//   res.end(JSON.stringify(x));
-// });
+  // Tutaj powinien po odebraniu zapytania odsylac juz poprawnie zapisanego jsona (z dodanym polem id, itp.)
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(tasks1));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-// abc
