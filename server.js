b0dedd1d0cd559102b4d69aecb8cf387ab705840
Piotr Florczak <piotrflorczakk@gmail.com>
2019-07-03 11:16:59 +0200

    Dodanie zapytaniom możliwości działania na bazie

--- server.js
+++ server.js
@@ -16,87 +16,52 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(bodyParser());
 
-// Podlaczenie bazy
-let db = new sqlite3.Database("frontendDB.sqlite3", err => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log("Connected to the in-memory SQlite database.");
-});
-
-const sql = "SELECT * FROM tasks ORDER BY id DESC";
-const tasks = [];
-
-db.all(sql, [], (err, rows) => {
-  if (err) {
-    throw err;
-  }
-  rows.forEach(row => {
-    tasks.push({
-      id: row.id,
-      actionDate: row.action_date,
-      createdAt: row.created_at,
-      deletedAt: row.deleted_at,
-      task: row.task,
-      comment: row.comment,
-      expense: row.expense,
-      quantity: row.quantity,
-      metalType: row.metal_type
-    });
-  });
-});
-
-// close the database connection
-db.close(err => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log("Close the database connection.");
-});
-
-const tasks1 = [
-  {
-    id: 1,
-    actionDate: "21.06.2019",
-    createdAt: "",
-    deletedAt: "",
-    task: "zakup",
-    comment: "",
-    expense: "2251",
-    quantity: "151",
-    metalType: "stalowy",
-    origin: "Sklep 1"
-  },
-  {
-    id: 2,
-    actionDate: "21.06.2019",
-    createdAt: "",
-    deletedAt: "",
-    task: "odbior",
-    comment: "",
-    expense: "1201",
-    quantity: "51",
-    metalType: "stalowy",
-    origin: "Sklep 1"
-  },
-  {
-    id: 3,
-    actionDate: "21.06.2019",
-    createdAt: "",
-    deletedAt: "",
-    task: "odbior",
-    comment: "",
-    expense: "3000",
-    quantity: "3000",
-    metalType: "stalowy",
-    origin: "Sklep 1"
-  }
-];
-
 // Zapytania
 app.get("/tasks", (req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(tasks));
+  const tasks = [];
+
+  // Podlaczenie bazy
+  let db = new sqlite3.Database("frontendDB.sqlite3", err => {
+    if (err) {
+      return console.error("err.message");
+    }
+  });
+
+  const sql = "SELECT * FROM tasks ORDER BY id DESC";
+
+  const retrunAllRows = new Promise((resolve, reject) => {
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      rows.forEach(row => {
+        tasks.push({
+          id: row.id,
+          actionDate: row.action_date,
+          createdAt: row.created_at,
+          deletedAt: row.deleted_at,
+          task: row.task,
+          comment: row.comment,
+          expense: row.expense,
+          quantity: row.quantity,
+          metalType: row.metal_type
+        });
+      });
+      resolve(tasks);
+    });
+  });
+
+  // Close the database connection
+  db.close(err => {
+    if (err) {
+      return console.error(err.message);
+    }
+  });
+
+  retrunAllRows.then(value => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(value));
+  });
 });
 
 app.post("/tasks", urlencodedParser, (req, res) => {
@@ -105,8 +70,61 @@ app.post("/tasks", urlencodedParser, (req, res) => {
   console.log(req.body);
 
   // Tutaj powinien po odebraniu zapytania odsylac juz poprawnie zapisanego jsona (z dodanym polem id, itp.)
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(tasks1));
+  const tasks = [];
+
+  // Podlaczenie bazy
+  let db = new sqlite3.Database("frontendDB.sqlite3", err => {
+    if (err) {
+      return console.error("err.message");
+    }
+  });
+
+  const rb = req.body;
+  const createdAt = new Date();
+
+  db.run(
+    `INSERT INTO tasks (action_date, created_at, task, comment, expense, quantity, metal_type, origin) VALUES ('${
+      rb.actionDate
+    }', '${createdAt}', '${rb.task}', '${rb.comment}', '${rb.expense}', '${
+      rb.quantity
+    }', '${rb.metalType}', "Sklep 1")`
+  );
+
+  const sql = "SELECT * FROM tasks ORDER BY id DESC";
+
+  const retrunAllRows = new Promise((resolve, reject) => {
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      rows.forEach(row => {
+        tasks.push({
+          id: row.id,
+          actionDate: row.action_date,
+          createdAt: row.created_at,
+          deletedAt: row.deleted_at,
+          task: row.task,
+          comment: row.comment,
+          expense: row.expense,
+          quantity: row.quantity,
+          metalType: row.metal_type
+        });
+      });
+      resolve(tasks);
+    });
+  });
+
+  // Close the database connection
+  db.close(err => {
+    if (err) {
+      return console.error(err.message);
+    }
+  });
+
+  retrunAllRows.then(value => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(value));
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
