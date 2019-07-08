5fb083308fe8da486b5f4d58bd9825b40191be89
Piotr Florczak <piotrflorczakk@gmail.com>
2019-07-08 17:11:27 +0200

    Pierwsze modyfikacje w Promises, dodanie walidacji

--- server.js
+++ server.js
@@ -5,30 +5,12 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3001;
 
-const sampleAllTasks = [
-  {
-    id: 1,
-    actionDate: "01.01.1000",
-    createdAt: "",
-    deletedAt: "",
-    task: "zakup",
-    comment: "",
-    expense: 0,
-    quantity: 0,
-    metalType: "stalowy"
-  },
-  {
-    id: 2,
-    actionDate: "01.01.1000",
-    createdAt: "[data dodana przez zapytanie POST]",
-    deletedAt: "",
-    task: "zakup",
-    comment: "",
-    expense: 0,
-    quantity: 0,
-    metalType: "stalowy"
-  }
-];
+function sendError(res, err) {
+  console.error(err.message);
+  console.trace();
+  // Przeslanie informacji o bledzie
+  res.status(500).end(err.message);
+}
 
 // "Rozpakowywanie" przychodzacych zapytan
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
@@ -54,11 +36,10 @@ app.get("/tasks", (req, res) => {
   const sql = "SELECT * FROM tasks ORDER BY id DESC";
   // Pobranie z bazy calej tabeli
   new Promise((resolve, reject) => {
-    // Przepisanie tabeli do tablicy
+    // Wykonanie operacji na tabeili
     db.all(sql, [], (err, rows) => {
-      if (err) {
-        reject();
-      } else {
+      if (err === null) {
+        // Przepisanie tabeli do tablicy
         const tasks = rows.map(row => {
           return {
             id: row.id,
@@ -73,6 +54,8 @@ app.get("/tasks", (req, res) => {
           };
         });
         resolve(tasks);
+      } else {
+        reject(err);
       }
     });
   })
@@ -80,67 +63,76 @@ app.get("/tasks", (req, res) => {
       res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify(value));
     })
-    .catch(() => {
-      res.status(400);
-      res.send();
+    .catch(err => {
+      sendError(res, err);
     });
 });
-
 app.post("/tasks", (req, res) => {
-  new Promise(resolve => {
+  new Promise((resolve, reject) => {
     // Dodac walidacje do req.body (dodatkowo zaokraglac do dwoch miejsc)!
     const rb = req.body;
-    const createdAt = new Date();
-    const sql = `INSERT INTO tasks (action_date, created_at, task, comment, expense, quantity, metal_type, origin) VALUES ('${
-      rb.actionDate
-    }', '${createdAt}', '${rb.task}', '${rb.comment}', '${rb.expense}', '${
-      rb.quantity
-    }', '${rb.metalType}', "Sklep 1")`;
+    const newInsertData = [
+      rb.actionDate,
+      new Date(),
+      rb.task,
+      rb.comment,
+      rb.expense,
+      rb.quantity,
+      rb.metalType,
+      "Sklep 1"
+    ];
+    // W miejscu ?-ikow wpisywane sa elementy tablicy newInsertData w funcki db.run
+    const sql =
+      "INSERT INTO tasks (action_date, created_at, task, comment, expense, quantity, metal_type, origin) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
     // Dodanie do bazy rekordu
-    db.run(sql, function(err) {
+    // Music tutaj byc function(err) {...}, a nie (err) => {...}, poniewaz w drugim przypadku this.lsatID jest undefined
+    db.run(sql, newInsertData, function(err) {
       // Zwrocenie id z ostatnio dodanego rekordu
       if (err === null) {
         resolve(this.lastID);
       } else {
-        console.log("Error");
+        reject(err);
       }
     });
-  }).then(lastRowId => {
-    const sql = `SELECT * FROM tasks WHERE id = '${lastRowId}'`;
-
-    // Pobranie z bazy calej tabeli
-    new Promise(resolve => {
-      // Przepisanie tabeli do tablicy
-      db.all(sql, [], (err, rows) => {
-        if (err) {
-          reject();
-        } else {
-          const tasks = rows.map(row => {
-            return {
-              id: row.id,
-              actionDate: row.action_date,
-              createdAt: row.created_at,
-              deletedAt: row.deleted_at,
-              task: row.task,
-              comment: row.comment,
-              expense: row.expense,
-              quantity: row.quantity,
-              metalType: row.metal_type
-            };
-          });
-          resolve(tasks);
-        }
-      });
-    })
-      .then(value => {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify(value));
+  })
+    .then(lastRowId => {
+      // Pobranie z bazy calej tabeli
+      new Promise(resolve => {
+        // W miejscu ? wpisywana jest wartosc lastRowId w funckji db.all
+        const sql = "SELECT * FROM tasks WHERE id = ?";
+        // Przepisanie tabeli do tablicy
+        db.all(sql, lastRowId, (err, rows) => {
+          if (err) {
+            reject(err);
+          } else {
+            const tasks = rows.map(row => {
+              return {
+                id: row.id,
+                actionDate: row.action_date,
+                createdAt: row.created_at,
+                deletedAt: row.deleted_at,
+                task: row.task,
+                comment: row.comment,
+                expense: row.expense,
+                quantity: row.quantity,
+                metalType: row.metal_type
+              };
+            });
+            resolve(tasks);
+          }
+        });
       })
-      .catch(() => {
-        res.status(400);
-        res.send();
-      });
-  });
+        .then(value => {
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end(JSON.stringify(value));
+        })
+        .catch(err => {
+          sendError(res, err);
+        });
+    })
+    .catch(err => {
+      sendError(res, err);
+    });
 });
 
 app.delete("/tasks/:id", (req, res) => {
